a54cc347e4a871f85616c121bfe961118c3d4360
dcchoi <dcchoi1234@gmail.com>
2017-04-12 22:35:03 -0400

    Spanning! (Splitting)

--- scala/run-length-encoding/src/main/scala/RunLengthEncoding.scala
+++ scala/run-length-encoding/src/main/scala/RunLengthEncoding.scala
@@ -3,37 +3,36 @@ import scala.annotation.tailrec
 object RunLengthEncoding {
 
   def encode(str: String): String = {
-
     @tailrec
-    def auxEncode(str: String, currentLetter: String, count: Int,result: String): String = {
-      if(str == ""){
-        if(count == 1) return result + currentLetter
-        else return result + count.toString + currentLetter
-      }
-      if(str.head.toString == currentLetter) return auxEncode(str.tail,currentLetter,count + 1,result)
-      else if(str.head == ' ') return auxEncode(str.tail,"", 1 ,result + count.toString + currentLetter + " ")
-      else{
-        if(count == 1) return auxEncode(str.tail,str.head.toString,1,result + currentLetter)
-        else return auxEncode(str.tail,str.head.toString,1,result + count.toString + currentLetter)
-      }
-    }
-    auxEncode(str, "", 1, "")
-  }
-
-  def decode(str: String): String = {
-
-    @tailrec
-    def auxDecode(str: String, currentDigit: String, result: String): String ={
-      if(str == "") result
-      else if(str.head.isDigit){
-        auxDecode(str.tail,currentDigit + str.head.toString, result)
-      }else{
-        if(currentDigit == "") auxDecode(str.tail, "", result + str.head)
-        else {
-          auxDecode(str.tail, "", result + (str.head.toString() * currentDigit.toInt))
+    def auxEncode(str:String, result: String): String = {
+      str.toList match {
+        case Nil => result
+        case head :: tail => {
+          auxEncode(str.span(_ == str.head)._2, result + (str.span(_ == str.head)._1.length match {
+            case 1 => ""
+            case x => x.toString
+          }) + str.head )
         }
       }
     }
-    auxDecode(str,"","")
+    auxEncode(str, "")
+  }
+
+
+
+  def decode(str: String): String = {
+    @tailrec
+    def auxDecode(str:String, result: String): String = {
+      str.toList match {
+        case Nil => result
+        case head :: tail => {
+          auxDecode(str.span(_.isDigit)._2.tail, result + (str.span(_.isDigit)._1 match {
+            case "" => str.span(_.isDigit)._2.head.toString()
+            case x => str.span(_.isDigit)._2.head.toString() * x.toInt
+          }))
+        }
+      }
+    }
+    auxDecode(str, "")
   }
 }
